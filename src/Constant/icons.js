18a55c384990866f18c4e3e7f4ffc3e938a1d807
ElRobertRocha91 <robertoestebanrocha91@gmail.com>
2024-08-24 19:20:20 -0300

    Add Object Constant

---
+++ src/Constant/icons.js
@@ -0,0 +1,28 @@
+const icons = [
+    {
+        id: 0,
+        name: "LinkedIn",
+        icon: "logo-linkedin",
+        link: "https://www.linkedin.com/in/roberto-esteban-rocha"
+    },
+    {
+        id: 1,
+        name: "GitHub",
+        icon: "logo-github",
+        link: "https://github.com/ElRobertRocha91"
+    },
+    {
+        id: 2,
+        name: "Gmail",
+        icon: "logo-google",
+        link: "robertoestebanrocha91@gmail.com"
+    },
+    {
+        id: 3,
+        name: "WhatsApp",
+        icon: "logo-whatsapp",
+        link: "1136482123"
+    }
+];
+
+export default icons;
