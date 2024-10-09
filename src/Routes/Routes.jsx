d4dcabfdb8e49e180099f110df8e5562de1d06f0
ElRobertRocha91 <robertoestebanrocha91@gmail.com>
2024-10-09 17:43:18 -0300

    Adding new route and page error

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -4,6 +4,8 @@ import Home from "../Pages/Home/Home";
 import Detail from "../Pages/Detail/Detail";
 import Form from "../Pages/Form/Form";
 import About from "../Pages/About/About";
+import Favorites from "../Pages/Favorites/Favorites";
+import Error from "../Pages/Error/Error";
 
 function Router() {
     return (
@@ -14,6 +16,9 @@ function Router() {
                     <Route path="/pokemon/:id" element={<Detail />}/>
                     <Route path="/form" element={<Form />}/>
                     <Route path="/about" element={<About />}/>
+                    <Route path="/favorites" element={<Favorites />}/>
+                    {/* Error Route: Not Found */}
+                    <Route path="*" element={<Error />}/>
             </Routes>
         </div>
     )
